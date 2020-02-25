b4168f65756914d1371c48c42be72f97d28e0db4
MuhibWaliShah <61011105+MuhibWaliShah@users.noreply.github.com>
2020-02-25 16:07:57 +0500

    Add files via upload

---
+++ introduction.dart
@@ -0,0 +1,17 @@
+void main(){
+  print("Hello World");
+
+  int num=100;
+  double percent=89.9;
+  String Name="Hello World";
+  var num1;
+  bool condition = false;
+  bool condition2 = true;
+  print(num);
+  print(percent);
+  print(Name);
+  print(num1=5.6);
+  print(condition);
+  print(condition2);
+  print(5+3);
+}
